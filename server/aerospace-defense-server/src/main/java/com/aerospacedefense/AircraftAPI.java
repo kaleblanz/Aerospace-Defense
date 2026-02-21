c6ece815d398ba43707ce291717d52a504e6bfed
Kaleb Lanz <kglanz2005@icloud.com>
2026-02-21 16:40:19 -0500

    Basic Endpoints added for functionality to handle web traffic coming to localhost:8080

---
+++ server/aerospace-defense-server/src/main/java/com/aerospacedefense/AircraftAPI.java
@@ -0,0 +1,43 @@
+package com.aerospacedefense;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//tells the class it should listen for web traffic
+@RestController
+public class AircraftAPI{
+    private AircraftHashMap aircraftHashMap;
+
+    public AircraftAPI(AircraftHashMap aircraftHashMap){
+        //the @Service tag on AircraftHashMap.java, Spring Boot injects this map
+        this.aircraftHashMap = aircraftHashMap;
+    }
+
+    // add our endpoints
+
+    //POST request at URL: add-aircraft
+    //@RequestBody deserializes the incoming HTTP Request (JSON) to the Aircraft Java object
+    @PostMapping("/add-aircraft")
+    public void endpointAddAircraft(@RequestBody Aircraft aircraft){
+        aircraftHashMap.addAircraft(aircraft);
+    }
+
+    // GET Request to recieve a specific Aircraft based on id
+    // PathVariable is to extract values from URI of incoming HTTP Request
+    @GetMapping("/get-aircraft/{id}")
+    public Aircraft endpointGetAircraft(@PathVariable int id){
+        //Spring Boot automatically serializes java into JSON
+        return aircraftHashMap.findAircraft(id);
+    }
+
+    // DeleteMapping is a route to delete
+    // DELETE Request to remove a specific Aircraft based on id
+    @DeleteMapping("/delete-aircraft/{id}")
+    public void endpointRemoveAircraft(@PathVariable int id){
+        aircraftHashMap.removeAircraft(id);
+    }
+}
